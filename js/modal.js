7db6ef4281af64d77d818b1e09bf325b990d38d7
Gabriel Leonardo Dias <116768323+Dias019@users.noreply.github.com>
2024-03-27 17:58:56 -0300

    first commit

---
+++ js/modal.js
@@ -0,0 +1,50 @@
+// Abrir e fechar Botão do modal
+const button = document.getElementById("botao")
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
+
+button.onclick = function (){
+    modal.showModal()
+}
+
+buttonClose.onclick = function (){
+    modal.close()
+}
+
+// Abrir e fechar Botão do modal 2
+const button2 = document.getElementById("botao2")
+const modal2 = document.getElementById("modal2")
+const buttonClose2 = document.getElementById("close2")
+
+button2.onclick = function (){
+    modal2.showModal()
+}
+
+buttonClose2.onclick = function (){
+    modal2.close()
+}
+// Abrir e fechar Botão do modal 3
+const button3 = document.getElementById("botao3")
+const modal3 = document.getElementById("modal3")
+const buttonClose3 = document.getElementById("close3")
+
+button3.onclick = function (){
+    modal3.showModal()
+}
+
+buttonClose3.onclick = function (){
+    modal3.close()
+}
+
+// Abrir e fechar Botão do modal 4
+const button4 = document.getElementById("botao4")
+const modal4 = document.getElementById("modal4")
+const buttonClose4 = document.getElementById("close4")
+
+button4.onclick = function (){
+    modal4.showModal()
+}
+
+buttonClose4.onclick = function (){
+    modal4.close()
+}
