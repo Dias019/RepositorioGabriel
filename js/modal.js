c6dce4935c90d056c27a936eea55417dedda512b
Gabriel Leonardo Dias <116768323+Dias019@users.noreply.github.com>
2024-08-13 13:00:53 -0300

    atualizaão repositório (blog e agenda PHP)

--- js/modal.js
+++ js/modal.js
@@ -1,50 +1,63 @@
 // Abrir e fechar Botão do modal
-const button = document.getElementById("botao")
-const modal = document.querySelector("dialog")
-const buttonClose = document.querySelector("dialog button")
+const button = document.getElementById("botao");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
 
-button.onclick = function (){
-    modal.showModal()
-}
+button.onclick = function () {
+  modal.showModal();
+};
 
-buttonClose.onclick = function (){
-    modal.close()
-}
+buttonClose.onclick = function () {
+  modal.close();
+};
 
 // Abrir e fechar Botão do modal 2
-const button2 = document.getElementById("botao2")
-const modal2 = document.getElementById("modal2")
-const buttonClose2 = document.getElementById("close2")
+const button2 = document.getElementById("botao2");
+const modal2 = document.getElementById("modal2");
+const buttonClose2 = document.getElementById("close2");
 
-button2.onclick = function (){
-    modal2.showModal()
-}
+button2.onclick = function () {
+  modal2.showModal();
+};
 
-buttonClose2.onclick = function (){
-    modal2.close()
-}
+buttonClose2.onclick = function () {
+  modal2.close();
+};
 // Abrir e fechar Botão do modal 3
-const button3 = document.getElementById("botao3")
-const modal3 = document.getElementById("modal3")
-const buttonClose3 = document.getElementById("close3")
+const button3 = document.getElementById("botao3");
+const modal3 = document.getElementById("modal3");
+const buttonClose3 = document.getElementById("close3");
 
-button3.onclick = function (){
-    modal3.showModal()
-}
+button3.onclick = function () {
+  modal3.showModal();
+};
 
-buttonClose3.onclick = function (){
-    modal3.close()
-}
+buttonClose3.onclick = function () {
+  modal3.close();
+};
 
 // Abrir e fechar Botão do modal 4
-const button4 = document.getElementById("botao4")
-const modal4 = document.getElementById("modal4")
-const buttonClose4 = document.getElementById("close4")
+const button4 = document.getElementById("botao4");
+const modal4 = document.getElementById("modal4");
+const buttonClose4 = document.getElementById("close4");
 
-button4.onclick = function (){
-    modal4.showModal()
-}
+button4.onclick = function () {
+  modal4.showModal();
+};
 
-buttonClose4.onclick = function (){
-    modal4.close()
-}
+buttonClose4.onclick = function () {
+  modal4.close();
+};
+
+// Abrir e fechar Botão do modal 5
+const button5 = document.getElementById("botao5");
+const modal5 = document.getElementById("modal5");
+const buttonClose5 = document.getElementById("close5");
+
+button5.onclick = function () {
+  modal5.showModal();
+};
+
+buttonClose5.onclick = function () {
+  modal5.close();
+};
